84554f8f154950d00cea1eafdc7c586af8c51aa5
Morilence <1280659615@qq.com>
2020-03-31 14:39:47 +0800

    style: some adjustments

--- src/string matching/kmp.js
+++ src/string matching/kmp.js
@@ -13,8 +13,8 @@
  */
 
 function kmp (text, pattern, overlap=false) {
-    let fail = [];
-    let endPosOfMaxPrefix = -1; // end position of max-prefix in pattern
+    let fail = [],
+    endPosOfMaxPrefix = -1; // end position of max-prefix in pattern
     fail[0] = endPosOfMaxPrefix;
     for (let i = 1; i < pattern.length; i++) {
         while (endPosOfMaxPrefix > -1 && pattern[endPosOfMaxPrefix+1] != pattern[i]) {
@@ -25,9 +25,8 @@ function kmp (text, pattern, overlap=false) {
         }
         fail[i] = endPosOfMaxPrefix;
     }
-
-    let locs = [];
-    let endPosOfMatched = -1;
+    let locs = [],
+    endPosOfMatched = -1;
     for (let i = 0; i < text.length; i++) {
         while (endPosOfMatched > -1 && pattern[endPosOfMatched+1] != text[i]) {
             endPosOfMatched = fail[endPosOfMatched];
